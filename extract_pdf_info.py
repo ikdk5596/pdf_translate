7eb189f9e1636756dc4c5dc3b0b5e019d025ea8d
Sanghong Kim <ikdk5559@naver.com>
2021-05-13 09:27:51 +0900

    Create extract_pdf_info.py

---
+++ extract_pdf_info.py
@@ -0,0 +1,30 @@
+# Extract pdf infomation for translate pdfs.
+# Author : sanghong kim
+# Organization : Repeblic of Korean Army
+# Reference : https://realpython.com/pdf-python/
+
+from PyPDF2 import PdfFileReader
+
+def extract_information(pdf_path):
+  with open(pdf_path, 'rb') as f:
+    pdf = PdfFileReader(f)
+    information = pdf.getDocumentInfo()
+    number_of_pages = pdf.getNumPages()
+    
+  txt = f"""
+  Information about {pdf_path}:
+  
+  Author: {information.author}
+  Creator: {information.creator}
+  Producer: {information.producer}
+  Subject: {information.subject}
+  Title: {information.title}
+  Number of pages: {number_of_pages}
+  """
+    
+  return txt, information
+
+if __name__ == '__main__':
+  path = 'sample.pdf'
+  txt, information = extract_information(path)
+  print(txt)

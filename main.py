80ba972963939542c1a0f583910beca4ad97c27d
Sanghong Kim <ikdk5559@naver.com>
2021-05-13 10:08:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+# Extract pdf infomation for translate pdfs.
+# Author : sanghong kim
+# Organization : Repeblic of Korean Army
+# Reference : https://realpython.com/pdf-python/
+
 import argparse
+from PyPDF2 import PdfFileReader
 from extract_pdf_info import extract_information
+
 
 def main(args):
   pdf_path = args.path
@@ -14,7 +21,6 @@ def main(args):
   text = page_content.encode('utf-8')
   print(text)
   
-
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()

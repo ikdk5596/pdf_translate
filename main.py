24d5961f9c46f0f673639801112b41f37450f216
Sanghong Kim <ikdk5559@naver.com>
2021-05-14 12:01:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,27 @@
 # 이후 Regular expression을 사용하여 replace 대체
 
 import argparse
+from . import layout_scanner
 from translate import translate
 
 
 def main(args):
-  pdf_path = args.path
-  origin, result = translate(pdf_path)
-  print(origin, ' -> ', result)
+  input_path = args.input
+  output_path = args.output
+  result = layout_scanner.get_pages(input_path)
+  with open(args.output, 'w') as f:
+    f.write(''.join(result))
+  # origin, result = translate(input_path)
+  # print(origin, ' -> ', result)
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--path', dest='path', default='sample.pdf')
+  parser = argparse.ArgumentParser(description = 'PDF to TEXT converter',
+                                  prog='python -m pdf_layout_scanner')
+  parser.add_argument('--input', type=str, dest='input', default='sample.pdf',
+                     help='input PDF file')
+  parser.add_argument('--output', type=str, dest='output', default='output.pdf',
+                      help='output PDF file')
   args = parser.parse_args()
   
   main(args)

a2a2e0d5eb7576c7e6fdddea5dc11f47d398ca39
Sanghong Kim <ikdk5559@naver.com>
2021-05-14 12:38:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ def main(args):
   input_path = args.input
   output_path = args.output
   result = layout_scanner.get_pages(input_path)
-  p.PdfFileWriter.write(output_path, result)
+  layout_scanner.write_file('./', output_path, result)
+  # p.PdfFileWriter.write(output_path, result)
   
   # origin, result = translate(input_path)
   # print(origin, ' -> ', result)

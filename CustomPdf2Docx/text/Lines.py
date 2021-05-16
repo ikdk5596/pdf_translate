2b05482cfe42f23ab1d39dc746f74794bbe80104
Sanghong Kim <ikdk5559@naver.com>
2021-05-16 14:44:43 +0900

    Update Lines.py

--- CustomPdf2Docx/text/Lines.py
+++ CustomPdf2Docx/text/Lines.py
@@ -79,7 +79,7 @@ class Lines(ElementCollection):
         for i in range(1, len(self._instances)):
             pre_line, line = self._instances[i-1], self._instances[i]
             
-            if line.text != '<image>'
+            if line.text != '<image>':
                 print(line.text)
             # ignore this line if overlap with previous line
             if line.get_main_bbox(pre_line, threshold=line_overlap_threshold):

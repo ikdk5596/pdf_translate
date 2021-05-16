2d32c288068432a80394c7ab1f10c617ec864710
Sanghong Kim <ikdk5559@naver.com>
2021-05-16 12:47:10 +0900

    Update PDFScanner.py

--- PDFScanner.py
+++ PDFScanner.py
@@ -1,37 +1,16 @@
-from pdfminer.pdfparser import PDFParser
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextContainer, LAParams
+from pdfminer.converter import TextConverter
 from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfpage import PDFPage, PDFTextExtractionNotAllowed
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfdevice import PDFDevice
-from pdfminer.layout import LAParmas, LTFigure, LTTextBoxHorizontal
-from pdfminer.converter import PDFPageAggregator
+from pdfminer.pdfinterp import PDFResouceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
 
-input = 'sample.pdf'
-f = open(input, 'rb')
-parser = PDFParser(f)
-document = PDFDocument(parser)
-if not document.is_extractable:
-  raise PDFTextExtractionNotAllowed
+def scan_pdf(input_pdf):
+  with open(input_pdf, 'rb') as f:
+    parser = PDFParser(f)
+    doc = PDFDocument(parser)
+    rsrcmqr = PDFResourceManager()
+    device = TextConverter(rsrcmqr, output_string, laparams=LAParams())
+    interpreter = PDFPageInterpreter(rsrcmqr, device)
     
-rsrcmgr = PDFResourceManager()
-device = PDFDevice(rsrcmgr)
-  
-laparams = LAParams()
-  
-device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-interpreter = PDFPageInterpreter(rsrcmgr, device)
-  
-def parse_obj(lt_objs): 
-  
-  for obj in lt_objs:
-    if isinstance(obj, LTTextBoxHorizontal):
-      print ("%6d, %6d, %s" % (obj.bbox[0], obj.bbox[1], obj.get_text().replace('\n', '_')))
-
-    elif isinstance(obj, LTFigure):
-      parse_obj(obj._objs)
-      
-for page in PDFPage.create_pages(document):
-  interpreter.process_page(page)
-  layout = device.get_result()
-  
-  parse_obj(layout._objs)

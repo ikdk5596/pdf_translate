b1ace778c19123946a931fc5f489734532948597
Sanghong Kim <ikdk5559@naver.com>
2021-05-16 17:06:09 +0900

    Update Blocks.py

--- CustomPdf2Docx/layout/Blocks.py
+++ CustomPdf2Docx/layout/Blocks.py
@@ -559,7 +559,7 @@ class Blocks(ElementCollection):
                 blocks.append(block)
             else:
                 text_block = TextBlock()
-                print(lines)
+                print(lines.line)
                 text_block.add(lines)
                 blocks.append(text_block)
 

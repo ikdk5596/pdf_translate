82541ed75bc4b66088aa19165135b2773a4cac04
Sanghong Kim <ikdk5559@naver.com>
2021-05-16 16:58:08 +0900

    Update TextBlock.py

--- CustomPdf2Docx/text/TextBlock.py
+++ CustomPdf2Docx/text/TextBlock.py
@@ -48,7 +48,7 @@ class TextBlock(Block):
 
         # collect lines
         self.lines = Lines(parent=self).restore(raw.get('lines', []))
-        print(self.lines)
+        print(self.lines.text)
         # set type
         self.set_text_block()
         

17a05e6c8c95a38e7a7beec3a4de68772c92809f
Sanghong Kim <ikdk5559@naver.com>
2021-05-16 18:39:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,44 @@ from pdfminer.pdfpage import PDFPage
 from docx import Document
 from google_trans_new import google_translator
 import os
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import Paragraph
+from reportlab.platypus import SimpleDocTemplate
 
+
+def get_translated_page_content(reader, lang):
+    """
+    Reads page content from the reader, translates it,
+    cleans it and returns page content as a list of strings.
+    Each entry in list represents a page
+    """
+    num_pages = reader.numPages
+    page_contents = []
+    translator = google_translator()
+    for p in range(num_pages):
+        page = reader.getPage(p)
+        text = page.extractText()
+        translation = translator.translate(text, lang_tgt='ko')
+        result_text = translation.text.replace("\n", " ").replace("W", "")
+        page_contents.append(result_text)
+    return page_contents
+  
+  
+def translate_pdf(path, lang):
+    file = open(path, 'rb')
+    reader = PyPDF2.PdfFileReader(file)
+    page_contents = get_translated_page_content(reader, lang)
+
+    page_text = []
+    name = f'{LANG}_{path}'
+    pdf = SimpleDocTemplate(name, pagesize=letter)
+
+    for text in page_contents:
+        page_text.append(
+            Paragraph(text, encoding='utf-8', style=regular))
+
+    pdf.build(page_text)
+  
   
 def main(args):
   input_path = args.input
@@ -25,8 +62,8 @@ def main(args):
   output_path = args.output
   sourceLanguageCode = args.sourceLanguageCode
   targetLanguageCode = args.targetLanguageCode
- 
-  translator = google_translator()
+  
+  translate_pdf(input_path, targetLanguageCode)
   
   with open(input_path, 'rb') as pdf_file:
     read_pdf = PyPDF2.PdfFileReader(pdf_file)
@@ -34,10 +71,8 @@ def main(args):
   if os.path.exists(output_path): os.remove(output_path)
   # convert pdf 2 docx
   parse(input_path, docx_path, start = 0, end = number_of_pages)
-  doc = Document(docx_path)
-  all_paras = doc.paragraphs
-  for para in all_paras:
-    print(para.text)
+
+  
 
 
 if __name__ == '__main__':

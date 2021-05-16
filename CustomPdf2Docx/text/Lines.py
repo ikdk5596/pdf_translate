dad71c3173260ae50dfac26b95266ee5818b65ad
Sanghong Kim <ikdk5559@naver.com>
2021-05-16 15:08:27 +0900

    Update Lines.py

--- CustomPdf2Docx/text/Lines.py
+++ CustomPdf2Docx/text/Lines.py
@@ -82,9 +82,7 @@ class Lines(ElementCollection):
         candidates = [self._instances[0]] # first line
         for i in range(1, len(self._instances)):
             pre_line, line = self._instances[i-1], self._instances[i]
-            print(line.text)
-            line.text = "test"
-            print(line.text)
+            
             # ignore this line if overlap with previous line
             if line.get_main_bbox(pre_line, threshold=line_overlap_threshold):
                 print(f'Ignore Line "{line.text}" due to overlap')

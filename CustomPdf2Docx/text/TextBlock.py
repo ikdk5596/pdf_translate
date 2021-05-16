0264910bb0afb228622beef89c6134cd25ba346e
Sanghong Kim <ikdk5559@naver.com>
2021-05-16 16:29:22 +0900

    Update TextBlock.py

--- CustomPdf2Docx/text/TextBlock.py
+++ CustomPdf2Docx/text/TextBlock.py
@@ -29,6 +29,7 @@ from docx.shared import Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 
 from .Lines import Lines
+from google_trans_new import google_translator
 from ..common.share import RectType, TextDirection, TextAlignment
 from ..common.Block import Block
 from ..common.share import rgb_component_from_name
@@ -56,6 +57,7 @@ class TextBlock(Block):
     def text(self):
         '''Get text content in block, joning each line with ``\\n``.'''
         lines_text = [line.text for line in self.lines]
+        print(lines_text)
         return '\n'.join(lines_text)
 
     

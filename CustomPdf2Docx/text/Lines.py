b5792d254b6f9bed2e2dedcefca04d0baa962fb4
Sanghong Kim <ikdk5559@naver.com>
2021-05-16 17:20:34 +0900

    Update Lines.py

--- CustomPdf2Docx/text/Lines.py
+++ CustomPdf2Docx/text/Lines.py
@@ -131,7 +131,7 @@ class Lines(ElementCollection):
         candidates = [self._instances[0]] # first line
         for i in range(1, len(self._instances)):
             pre_line, line = self._instances[i-1], self._instances[i]
-            print(line)
+            print(line.text)
 
 
     def split_back(self):
